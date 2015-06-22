c7c163012dd186a7214716ca57223a05617da424
André Ericson <de.ericson@gmail.com>
2015-06-22 03:40:13 -0300

    Add comments for clarity

--- example/context_processors.py
+++ example/context_processors.py
@@ -2,4 +2,5 @@ from django.conf import settings
 
 
 def debug(context):
+    # allow to check DEBUG flag from a template
     return {'DEBUG': settings.DEBUG}
--- example/forms.py
+++ example/forms.py
@@ -11,25 +11,30 @@ from example.models import Document
 class DocumentForm(forms.ModelForm):
     file_name = forms.CharField(widget=forms.HiddenInput, required=False)
 
+    class Meta:
+        model = Document
+        fields = ('id', 'name', 'doc_file', 'file_name')
+
     def __init__(self, *args, **kwargs):
+        # crispy forms
         self.helper = FormHelper()
         self.helper.form_class = 's3upload-form'
         self.helper.add_input(Submit('submit', 'Submit'))
         super(DocumentForm, self).__init__(*args, **kwargs)
+        # doc_file is not required if file_name is set
         self.fields['doc_file'].required = False
 
     def clean(self):
         file_name = self.cleaned_data.get('file_name')
         doc_file = self.cleaned_data.get('doc_file')
+        # if doc_file and file_name is not set we are missing the file
         if not doc_file and not file_name:
             self.add_error(
                 'doc_file',
                 ValidationError(self.fields['doc_file']
                                 .error_messages['required'], code='required'))
+        # if filename is set with the path of the file it was uploaded by
+        # the frontend
         elif not doc_file and file_name:
             self.cleaned_data['doc_file'] = file_name
         return super(DocumentForm, self).clean()
-
-    class Meta:
-        model = Document
-        fields = ('id', 'name', 'doc_file', 'file_name')
